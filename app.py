d382e0e4ea1f7d17c855032fc43f60c14c919241
Ryam Lael <128926385+RyamLael@users.noreply.github.com>
2025-02-28 19:58:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,21 +14,3 @@ if __name__ == '__main__':
         db.create_all()
 
     app.run()
-
-
-#fazer o loggout do user
-#Normalizar os retornos (falso/None)
-#Padronizar partes do codigo
-#Adicionar exeptions específicas
-#Retornar os códigos de Erro HTTP no services
-#remover funções crud desnecessárias 
-#class meta no schema? NO
-#formatar a data e hora YES
-#Criar classe membro NO
-#faz sentido usar esses event repository por ai??? YES
-#Nas extensões adicionar as redundantes
-#padronizar os find pra get
-#o mesmo criador de evento nao pode ter dois eventos com nomes iguais
-#quero que vender o ingresso aumente o budget do evento
-#get upcomming activities es´ta quebrado
-#adicionar as categorias de eventos
