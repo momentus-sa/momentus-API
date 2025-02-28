3494006b7b192783e633dbb16735041f10cfbd06
Ryam Lael <128926385+RyamLael@users.noreply.github.com>
2025-02-28 19:56:43 -0300

    Update extensions.py

--- src/extensions.py
+++ src/extensions.py
@@ -3,8 +3,6 @@ from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 from flask_jwt_extended import JWTManager
 
-#tiro esse mashmallow?
-
 db = SQLAlchemy()
 migrate = Migrate()
 marshmallow = Marshmallow()
